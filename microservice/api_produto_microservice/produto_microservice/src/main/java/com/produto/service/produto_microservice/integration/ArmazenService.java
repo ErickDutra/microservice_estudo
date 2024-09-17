4235f9ba24b145b0bb49b897540920edbb357879
Erick Dutra <erickdutra140@gmail.com>
2024-09-17 20:50:20 -0300

    service

--- microservice/api_produto_microservice/produto_microservice/src/main/java/com/produto/service/produto_microservice/integration/ArmazenService.java
+++ microservice/api_produto_microservice/produto_microservice/src/main/java/com/produto/service/produto_microservice/integration/ArmazenService.java
@@ -1,6 +1,5 @@
 package com.produto.service.produto_microservice.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.produto.service.produto_microservice.integration.armazen.ArmazenProduto;
@@ -13,21 +12,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ArmazenService {
 
-<<<<<<< HEAD
-    @Autowired
-    private final ArmazenProduto armazenProduto;
 
-    public ArmazenDto postQuantity(ArmazenDto armazenDto) {
-        try {
-            return armazenProduto.postQuantity(armazenDto);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao postar quantidade", e);
-        }
-=======
     private ArmazenProduto armazenProduto;
 
     public void postQuantity(ArmazenDto armazenDto) {
         armazenProduto.postQuantity(armazenDto);
->>>>>>> aa55405700ac5828f009268a96285235397d99af
     }
 }
